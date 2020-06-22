482ce3c8c0ca0a3c2abdc0a3cd2c62aa7c75a496
Ines Dyll <inesdyll1@gmail.com>
2020-06-22 15:33:48 +0200

    json and routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+var express = require('express');
+
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pokemonRouter = require('./routes/pokemon');
 
 var app = express();
 
@@ -17,10 +19,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pokemon', pokemonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
